884508ac0099640263b7b6009973fa1d8b251bcf
Yoshi Gao <yoshi4868686@gmail.com>
2023-01-15 18:25:03 +0800

    Add unit test.

---
+++ tests/test_embedding_store.py
@@ -0,0 +1,46 @@
+from typing import List, Text
+
+import numpy as np
+import pytest
+
+from embedding_store.base import VALID_COLUMN_ATTRIBUTE, EmbeddingStore
+
+
+class MockEmbeddingStore(EmbeddingStore):
+    def _retrieve_embeddings_from_model(self, sentences: List[Text]) -> np.ndarray:
+        return np.ones((len(sentences), 768))
+
+
+@pytest.mark.unit
+def test_retrieve_embeddings():
+    query_sentences = ["I want some dinner", "Bella Chiao"]
+
+    mock_embedding_store = MockEmbeddingStore()
+    results = mock_embedding_store.retrieve_embeddings(query_sentences)
+
+    assert results.shape == (2, 768)
+    assert results[0].tolist() == np.ones(768).tolist()
+    assert results[1].tolist() == np.ones(768).tolist()
+
+
+@pytest.mark.unit
+def test_retrieve_dataframe_embeddings():
+    query_sentences = ["I want some dinner", "Bella Chiao"]
+
+    mock_embedding_store = MockEmbeddingStore()
+    results = mock_embedding_store.retrieve_dataframe_embeddings(query_sentences)
+
+    assert results.shape == (2, 1)
+    assert results[VALID_COLUMN_ATTRIBUTE].iloc[0].tolist() == np.ones(768).tolist()
+
+
+# @pytest.mark.integration
+# @pytest.mark.parametrize("embedding_store", ["clip_embedding_store"], indirect=True)
+# def test_retrieve_embeddings_from_external_source(embedding_store: EmbeddingStore):
+#     query_sentences = ["播放音樂", "".join([str(randrange(0, 10)) for _ in range(10)]), "我要聽音樂"]
+
+#     results = embedding_store.retrieve_dataframe_embeddings(sentences=query_sentences)
+
+#     assert results.shape == (3, 2)
+#     assert not any(map(lambda x: x is None, results[VALID_ROW_ATTRIBUTE].to_list()))
+#     assert not any(map(lambda x: x is None, results[VALID_COLUMN_ATTRIBUTE].to_list()))

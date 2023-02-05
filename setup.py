1ee00231ed6d3c68029d0cf1a43cffd6281dfbaa
Yoshi Gao <yoshi4868686@gmail.com>
2023-02-05 21:30:30 +0800

    prepared release of version 0.2.1

--- setup.py
+++ setup.py
@@ -14,41 +14,47 @@ extras_require = {
 
 setup_kwargs = {
     "name": "embestore",
-    "version": "0.2.0",
+    "version": "0.2.1",
     "description": "",
     "long_description": (
         "[![Code style:"
         " black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)\n\n##"
         " Introduction\n\nImplement the sentence embedding retriever with local cache from the embedding store.\n\n##"
-        " Features\n\n* Embedding store abstraction class\n\n* Support Jina client implementation embedding store\n\n*"
-        " Support LFU, LRU cache eviction policy for limited cache size, if the eviction policy is not specified then"
-        " won't\napply any eviction policy\n\n* Save the cache to parquet file\n\n* Load the cache from existed"
-        " parquet file\n\n## Quick Start\n\n### **Option 1.** Using Jina flow serve the embedding model\n\n*"
-        ' Installation\n\n```bash\npip install embestore"[jina]"\n```\n\n* To start up the Jina flow service with'
-        " sentence embedding model\n`sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2`, you can just"
-        " clone\nthis github repo directly and serve by the docker container.\n\n```bash\ngit clone"
+        " Features\n\n* Embedding store abstraction class\n\n* Support [Jina](https://github.com/jina-ai/jina) client"
+        " implementation embedding store\n\n* Support LFU, LRU cache eviction policy for limited cache size, if the"
+        " eviction policy is not specified then won't\napply any eviction policy\n\n* Save the cache to parquet"
+        " file\n\n* Load the cache from existed parquet file\n\n## Quick Start\n\n### **Option 1.** Using Jina flow"
+        ' serve the embedding model\n\n* Installation\n\n```bash\npip install embestore"[jina]"\n```\n\n* To start up'
+        " the Jina flow service with sentence embedding"
+        " model\n`sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2`, you can just clone\nthis github repo"
+        " directly and serve by the docker container.\n\n```bash\ngit clone"
         " https://github.com/ycc789741ycc/sentence-embedding-dataframe-cache.git\n\ncd"
         " sentence-embedding-dataframe-cache\n\nmake serve-jina-embedding\n```\n\n* Retrieve the"
-        " embedding\n\n```python\nfrom embestore.store.jina import JinaEmbeddingStore\n\nJINA_EMBESTORE_GRPC ="
+        " embedding\n\n```python\nfrom embestore.store.jina import JinaEmbeddingStore\n\nJINA_EMBEDDING_STORE_GRPC ="
         ' "grpc://0.0.0.0:54321"\n\n\nquery_sentences = ["I want to listen the music.", "Music don\'t want to listen'
-        ' me."]\n\njina_embestore = JinaEmbeddingStore(embedding_grpc=JINA_EMBESTORE_GRPC)\nresults ='
-        " jina_embestore.retrieve_embeddings(sentences=query_sentences)\n```\n\n* Stop the docker"
-        " container\n\n```bash\nmake stop-jina-embedding\n```\n\n### **Option 2.** Using local sentence embedding"
-        ' model\n\n* Installation\n\n```bash\npip install embestore"[sentence-transformers]"\n```\n\n* Serve the'
-        " sentence embedding model `sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2` by"
+        ' me."]\n\njina_embedding_store = JinaEmbeddingStore(embedding_grpc=JINA_EMBEDDING_STORE_GRPC)\nembeddings ='
+        " jina_embedding_store.retrieve_embeddings(sentences=query_sentences)\n\n>>> embeddings\narray([["
+        " 2.26917475e-01,  8.17841291e-02,  2.35427842e-02,\n        -3.02357599e-02,  1.15757119e-02,"
+        " -8.42996314e-02,\n         4.42815214e-01,  1.80795133e-01,  1.04702041e-01,\n         ...\n]])\n```\n\n*"
+        " Stop the docker container\n\n```bash\nmake stop-jina-embedding\n```\n\n### **Option 2.** Using local sentence"
+        ' embedding model\n\n* Installation\n\n```bash\npip install embestore"[sentence-transformers]"\n```\n\n* Serve'
+        " the sentence embedding model `sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2` by"
         ' in-memory\n\n```python\nfrom embestore.store.torch import TorchEmbeddingStore\n\nquery_sentences = ["I want'
-        ' to listen the music.", "Music don\'t want to listen me."]\n\n\ntorch_embestore ='
-        " TorchEmbeddingStore()\nresults = torch_embestore.retrieve_embeddings(sentences=query_sentences)\n```\n\n###"
+        ' to listen the music.", "Music don\'t want to listen me."]\n\n\ntorch_embedding_store ='
+        " TorchEmbeddingStore()\nembeddings ="
+        " torch_embedding_store.retrieve_embeddings(sentences=query_sentences)\n\n>>> embeddings\narray([["
+        " 2.26917475e-01,  8.17841291e-02,  2.35427842e-02,\n        -3.02357599e-02,  1.15757119e-02,"
+        " -8.42996314e-02,\n         4.42815214e-01,  1.80795133e-01,  1.04702041e-01,\n         ...\n]])\n```\n\n###"
         " **Option 3.** Inherit from the abstraction class\n\n* Installation\n\n```bash\npip install"
         " embestore\n```\n\n```python\nfrom typing import List, Text\n\nimport numpy as np\nfrom sentence_transformers"
         " import SentenceTransformer\n\nfrom embestore.store.base import EmbeddingStore\n\nmodel ="
         ' SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2").eval()\n\n\nclass'
         " TorchEmbeddingStore(EmbeddingStore):\n    def _retrieve_embeddings_from_model(self, sentences: List[Text]) ->"
         " np.ndarray:\n        return model.encode(sentences)\n```\n\n### Save the"
-        ' cache\n\n```python\ntorch_embestore.save("cache.parquet")\n```\n\n### Load from the'
-        ' cache\n\n```python\ntorch_embestore = TorchEmbeddingStore("cache.parquet")\n```\n\n### Apply eviction'
-        " policy\n\n* LRU\n\n```python\ntorch_embestore = TorchEmbeddingStore(max_size=100,"
-        ' eviction_policy="lru")\n```\n\n* LFU\n\n```python\ntorch_embestore = TorchEmbeddingStore(max_size=100,'
+        ' cache\n\n```python\ntorch_embedding_store.save("cache.parquet")\n```\n\n### Load from the'
+        ' cache\n\n```python\ntorch_embedding_store = TorchEmbeddingStore("cache.parquet")\n```\n\n### Apply eviction'
+        " policy\n\n* LRU\n\n```python\ntorch_embedding_store = TorchEmbeddingStore(max_size=100,"
+        ' eviction_policy="lru")\n```\n\n* LFU\n\n```python\ntorch_embedding_store = TorchEmbeddingStore(max_size=100,'
         ' eviction_policy="lfu")\n```\n\n## Road Map\n\n[TODO] Documentation\n\n[TODO] Badges\n'
     ),
     "author": "Yoshi Gao",
